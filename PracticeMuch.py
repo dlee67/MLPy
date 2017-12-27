9d6439c9b4ece7f0a5373b960044c076cb1b530a
bob <dlee67@msudenver.edu>
2017-12-27 16:06:57 -0700

    Such prac

--- PracticeMuch.py
+++ PracticeMuch.py
@@ -1,12 +1,13 @@
-to_ten = [1, 1, 1, 4, 5, 6, 7, 8, 9, 10]
+#Python has something called while-else
+#Pretty fancy
+to_ten = [1, 1, 1, 4, 6, 7, 8, 9, 10]
 counter = 0
 
 while counter != len(to_ten):
+	print("Got: ", to_ten[counter]) 
 	if to_ten[counter] == 5:
-		counter += 1
-		continue	
-	print(to_ten[counter])
-	if to_ten[counter] == 7:
-		break
+		print("Got: ", to_ten[counter], " Breaking out of the loop.")
+		break	
 	counter += 1
-	
+else:
+	print("Didn't got five :(")
