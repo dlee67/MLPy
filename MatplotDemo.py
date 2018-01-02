bf18e8820098b648ad0f71efa444375c271fb5fd
bob <dlee67@msudenver.edu>
2018-01-02 11:59:09 -0700

    Prac

--- MatplotDemo.py
+++ MatplotDemo.py
@@ -16,6 +16,12 @@ import matplotlib.pyplot as mp
 #diff_cost = 94000-86000
 #diff_size = 1900-1600
 
-#broken
+#I can't figure out how to make this thing look like the histogram from udacity: statistic 101,
+#Quiz: Histograms 2.
+#The frequencies appear correctly, but the representation is not intended.
 salaries = np.array([132754, 137192, 122177, 147121, 143000, 126010, 129200, 124312, 128132])
-#It might be a better idea to study more about that concepts and come back.
+mp.hist(salaries, [120000, 130000, 140000], range=None, density=None, weights=None, 
+cumulative=False, bottom=None, histtype='bar', align='mid', orientation='vertical', rwidth=None, log=False, 
+color=None, label=None, stacked=False, normed=None, hold=None, data=None, **kwargs)
+#The representations of the x-axis lables aren't how I want.
+mp.show()
